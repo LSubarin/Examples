830bc261e24de96ee46cf43a24be55d33e900d94
Aleksey Khorkov <60649756+LSubarin@users.noreply.github.com>
2023-03-05 19:18:56 +0300

    Example002_HelloUser. Вводим имя и приветствуем пользователя.

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.Write("Введите ваше имя "); // Приглашение ввести имя
+string? username = Console.ReadLine();     // Знак вопроса сообщает, что string может иметь значение NULL (быть пустой строкой)
+System.Console.Write("Привет, ");
+System.Console.WriteLine(username);
